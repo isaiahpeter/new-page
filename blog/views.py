933766eb60fc9f14d2c0097a1c83a0bfe7f3dd61
peter <isaiahjuniorp@gmail.com>
2021-09-29 05:52:26 +0100

    again

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView) # new
 from .models import Post
@@ -18,11 +18,17 @@ class BlogCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form): 
         form.instance.author = self.request.user
         return super().form_valid(form)
-class BlogUpdateView(LoginRequiredMixin, UpdateView): # new
+class BlogUpdateView(LoginRequiredMixin, UpdateView, UserPassesTestMixin): # new
     model = Post
     template_name = 'post_edit.html'
     fields = ('title', 'body')
-class BlogDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self): 
+        obj = self.get_object()
+        return obj.author == self.request.user
+class BlogDeleteView(LoginRequiredMixin, DeleteView, UserPassesTestMixin):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
