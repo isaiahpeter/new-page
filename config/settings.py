cd84730121ec99825b0aac68b70820edf8b42d65
peter <isaiahjuniorp@gmail.com>
2021-11-04 01:49:12 +0100

    redeployment

--- config/settings.py
+++ config/settings.py
@@ -152,9 +152,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
-    'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.google.GoogleOAuth2',
+  
 ]
 
 SOCIAL_AUTH_FACEBOOK_KEY = '' # Facebook App ID
